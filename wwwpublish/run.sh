0aff11b6b174b4bdbaff43a7eda0ec179838b588
Olaf Klischat <olaf.klischat@gmail.com>
2016-12-03 01:38:08 +0000

    wwwpublish: run in bkgnd

--- wwwpublish/run.sh
+++ wwwpublish/run.sh
@@ -3,5 +3,5 @@
 set -e
 
 docker build -t oklischat/wwwpublish:latest .
-docker run --rm --name=wwwpublish -v /media:/media -p 8080:80 oklischat/wwwpublish:latest
+docker run -d --name=wwwpublish -v /media:/media -p 8080:80 oklischat/wwwpublish:latest
 
